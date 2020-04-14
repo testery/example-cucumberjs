3c3b9052a8815110192f2a62c848e847c2dc922c
Aaron Freeman <aaron@freeman.zone>
2020-04-14 13:06:04 -0500

    adding cucumber stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# example-cucumberjs
+
+Example project for showing CucumberJS tests running on Testery.
+
+To run tests: `npm test`
---
+++ features/steps/navigation.js
@@ -0,0 +1,32 @@
+const { Builder, By, Capabilities, Key } = require("selenium-webdriver");
+const { When, Then, AfterAll } = require("cucumber");
+const { expect } = require("chai");
+const fs = require("fs");
+require("chromedriver");
+
+const capabilities = Capabilities.chrome();
+
+if (process.env.IS_TESTERY == "true") {
+  capabilities.set("chromeOptions", {
+    args: process.env.TESTERY_CHROME_ARGS.split(","),
+  });
+}
+const browser = new Builder().withCapabilities(capabilities).build();
+
+When("I navigate to {string}", async function (url) {
+  await browser.get(url);
+});
+
+Then("the page title is {string}", { timeout: 60 * 1000 }, async function (title) {
+  const expected = await browser.getTitle();
+  expect(title).to.equal(expected);
+});
+
+Then("take a screenshot", async function () {
+  const value = await browser.takeScreenshot();
+  fs.writeFileSync("./screenshots/" + Math.random() + ".png", value, "base64");
+});
+
+AfterAll("end", async function () {
+  await browser.quit();
+});
