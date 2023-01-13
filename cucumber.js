224e033a0892180cb578c946d07eb455cb561404
Will Gunn <will.gunn@testery.io>
2023-01-13 15:26:18 -0600

    try different folder structure

--- cucumber.js
+++ cucumber.js
@@ -1,3 +1,10 @@
 module.exports = {
-  default: `--format-options '{"snippetInterface": "synchronous"}'`,
-};
+    default: [
+      "first_part/features/**/*.feature",
+      "other_part/features/**/*.feature",
+      "--require-module ts-node/register",
+      "--require first_part/features/**/*.ts",
+      "--require other_part/features/**/*.ts",
+      "--publish-quiet",
+    ].join(" "),
+  };
---
+++ first_part/features/first_greetings.feature
@@ -0,0 +1,5 @@
+Feature: First Greeting
+
+	Scenario: Say hello
+		When the greeter says hello first
+		Then I should have heard "hello" first
