6a5c515db2750b8ff424a623e920300ac51fce7e
Will Gunn <will.gunn@testery.io>
2023-01-19 13:29:50 -0600

    fix other step file

--- other_part/features/step_defintions/steps.ts
+++ other_part/features/step_defintions/steps.ts
@@ -3,14 +3,10 @@ import assert from "assert";
 import { When, Then } from "@cucumber/cucumber";
 import { Greeter } from "../../../src";
 
-interface MyWorld {
-  whatIHeard: string;
-}
-
-When("the greeter again says hello", function (this: MyWorld) {
+When("the greeter again says hello", function () {
   this.whatIHeard = new Greeter().sayHello();
 });
 
-Then("I should have again heard hello", function (this: MyWorld) {
+Then("I should have again heard hello", function () {
     assert.equal(this.whatIHeard, "hello");
 });
